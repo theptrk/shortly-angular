df4d5d9b8932299eb52cfffcc3f0218b41dfaf3d
Hack Reactor Students <patrick.tran06@gmail.com>
2014-05-16 22:08:27 -0700

    Add ngRoute functions

--- public/client/js/app.js
+++ public/client/js/app.js
@@ -1,5 +1,5 @@
 // ANGULEUR
-angular.module('myApp', [])
+angular.module('myApp', ['ngRoute', 'ngAnimate', 'fx.animations'])
 
 .controller('linkController', function($scope, $http){
   $scope.name = "patrick"
@@ -15,15 +15,34 @@ angular.module('myApp', [])
 })
 
 .controller('submitController', function($scope, $http){
+  $scope.shouldShow = false;
   $scope.submit = function(){
     console.log($scope.text);
     console.log($scope);
+    $scope.shouldShow = true;
     $http({
       method:'POST',
       data:{url: $scope.text},
       url:'/links'
     }).success(function(data, status){
-      console.log('posted!')
+      console.log('posted!');
+      $scope.shouldShow = false;
+      $scope.text = '';
     }).error(function(data, status){});
   };
 })
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: '/client/templates/home.html', // add real url
+      controller: 'linkController' // add real controller
+    })
+    .when('/create', {
+      templateUrl: '/client/templates/shorten.html', // add real url
+      controller: 'submitController' // add real controller
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
+}]);
